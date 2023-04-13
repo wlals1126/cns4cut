43d13290cef10116b3f6dcec570e5d063be17d00
wlals1126 <b01040987587@gmail.com>
2023-04-13 23:08:02 +0900

    Update Main.jsx
    Update Main.jsx

--- v2/src/components/main/Main.jsx
+++ v2/src/components/main/Main.jsx
@@ -14,13 +14,13 @@ import schoolBackImg from "../../assets/dgswback.jpg";
 import schoolFrontImg from "../../assets/main.jpg";
 import playGroundImg from "../../assets/school.jpg";
 import setBackground from "../../assets/setBackground.svg";
-import setFilter from "../../assets/setFilter.svg"
+import setFilter from "../../assets/setFilter.svg";
 
 //import lupi from "../../assets/lupi.png"
-import dinosaur from "../../assets/dinosaur.jpg"
-import cb from "../../assets/cb.jpg"
-import jjanggu from "../../assets/jjanggu.png";
-import spongibab from "../../assets/spongibab.jpg"
+import dinosaur from "../../assets/dinosaur.jpg";
+import cb from "../../assets/cb.jpg";
+import jjanggu from "../../assets/jjanggu.jpg";
+import spongibab from "../../assets/spongibab.jpg";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from "recoil";
 import { imageState } from "../../global/image";
@@ -49,9 +49,8 @@ const Main = () => {
   }, [image]);
 
   useEffect(() => {
-    console.log(backImage)
-  }, [backImage])
-
+    console.log(backImage);
+  }, [backImage]);
 
   async function drawMask(
     webcam,
@@ -60,17 +59,19 @@ const Main = () => {
     tempCanvas,
     originCtx,
     originCanvas,
-    context,
+    context
   ) {
-    requestAnimationFrame(() => drawMask(
-      webcam,
-      canvas,
-      tempCtx,
-      tempCanvas,
-      originCtx,
-      originCanvas,
-      context,
-    ))
+    requestAnimationFrame(() =>
+      drawMask(
+        webcam,
+        canvas,
+        tempCtx,
+        tempCanvas,
+        originCtx,
+        originCanvas,
+        context
+      )
+    );
     const segmentation = await bodypixnet.segmentPerson(webcam);
     const mask = bodyPix.toMask(segmentation);
     tempCtx.putImageData(mask, 0, 0);
@@ -124,18 +125,18 @@ const Main = () => {
     //   }
     //   context.drawImage(originCanvas, 0, 0, canvas.width, canvas.height);
     // })();
-    req = requestAnimationFrame(() => drawMask(
-      webcam,
-      canvas,
-      tempCtx,
-      tempCanvas,
-      originCtx,
-      originCanvas,
-      context,
-    ))
+    req = requestAnimationFrame(() =>
+      drawMask(
+        webcam,
+        canvas,
+        tempCtx,
+        tempCanvas,
+        originCtx,
+        originCanvas,
+        context
+      )
+    );
   };
-
-
 
   const clickHandler = (backImgName) => {
     const webcam = webcamRef.current.video;
@@ -160,7 +161,7 @@ const Main = () => {
       //   setBackImage(img);
       // };
     } else {
-      setBackImage(null)
+      setBackImage(null);
     }
 
     // 바디픽서가 없을 땐 에러가 뜨기 때문에
@@ -182,7 +183,7 @@ const Main = () => {
       setImage((prev) => [...prev, canvasRef.current.toDataURL("image/jpeg")]);
     } else {
       // setImage((prev) => [...prev,webcamRef])
-      console.log(webcamRef.current.video.toDataURL("image/jpeg"))
+      console.log(webcamRef.current.video.toDataURL("image/jpeg"));
     }
   }
 
@@ -212,57 +213,54 @@ const Main = () => {
       </M.CamWrapper>
       <M.ButtonWrapper>
         <M.Header>
-
           <M.HeaderImg src={logo}></M.HeaderImg>
         </M.Header>
         <div>
+          <div>
+            <M.TextImg src={setBackground}></M.TextImg>
+            <div>
+              <M.Button onClick={() => clickHandler(dinosaur)}>
+                쥬라기 스쿨
+              </M.Button>
+              <M.Button onClick={() => clickHandler(spongibab)}>
+                스폰지밥
+              </M.Button>
+              <M.Button onClick={() => clickHandler(playGroundImg)}>
+                학교 운동장
+              </M.Button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <M.Button onClick={() => clickHandler(schoolFrontImg)}>
+                학교 기숙사동
+              </M.Button>
+              <M.Button onClick={() => clickHandler(schoolBackImg)}>
+                학교 크로마키
+              </M.Button>
+              <M.Button onClick={() => clickHandler(cb)}>벚꽃</M.Button>
+            </div>
+          </div>
+        </div>
         <div>
-          <M.TextImg src={setBackground}></M.TextImg>
+          <M.TextImg2 src={setFilter}></M.TextImg2>
           <div>
             <M.Button onClick={() => clickHandler(dinosaur)}>
               쥬라기 스쿨
             </M.Button>
-            <M.Button onClick={() => clickHandler(spongibab)}>
-              스폰지밥
-            </M.Button>
+            <M.Button onClick={() => clickHandler(jjanggu)}>짱구</M.Button>
             <M.Button onClick={() => clickHandler(playGroundImg)}>
               학교 운동장
             </M.Button>
           </div>
         </div>
-        <div>
-        <div>
-          <M.Button onClick={() => clickHandler(schoolFrontImg)}>
-            학교 기숙사동
-          </M.Button>
-          <M.Button onClick={() => clickHandler(schoolBackImg)}>
-            학교 크로마키
-          </M.Button>
-          <M.Button onClick={() => clickHandler(cb)}>
-            벚꽃
-          </M.Button>
-
-        </div>
-        </div>
-        </div>
-        <div>
-          <M.TextImg2 src={setFilter}></M.TextImg2>
-        <div>
-          <M.Button onClick={() => clickHandler(dinosaur)}>
-            쥬라기 스쿨
-          </M.Button>
-          <M.Button onClick={() => clickHandler(jjanggu)}>
-            짱구
-          </M.Button>
-          <M.Button onClick={() => clickHandler(playGroundImg)}>
-            학교 운동장
-          </M.Button>
-        </div>
-        </div>
 
         {/* <M.Button onClick={() => resetRAF()}>배경 없애기</M.Button> */}
-        <M.TakeButton src={takePicture} onClick={() => snapshot()}></M.TakeButton>
-        <img src={takePicture} onClick={() => snapshot()} ></img>
+        <M.TakeButton
+          src={takePicture}
+          onClick={() => snapshot()}
+        ></M.TakeButton>
+        <img src={takePicture} onClick={() => snapshot()}></img>
         <M.TakeButton src={takePicture}></M.TakeButton>
         {/* {
             image.map((e,idx) => (
